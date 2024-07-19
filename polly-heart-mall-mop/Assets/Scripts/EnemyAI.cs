7aeac6ca14130343e872648d138163ba01f8f34c
Kristen <kristenmarcinek@protonmail.com>
2024-07-18 20:53:23 -0400

    Revert "Merge branch 'main' into enemy-aggro"
    This reverts commit a299ed2cbe6c52d107b39b144721a663eb9d88af, reversing
    changes made to 327ebdfee5ced6c08f1a512e803f9dcdbcc140a4.

--- polly-heart-mall-mop/Assets/Scripts/EnemyAI.cs
+++ polly-heart-mall-mop/Assets/Scripts/EnemyAI.cs
@@ -6,9 +6,6 @@ public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
 
-    public Transform[] patrolPoints;
-    public float moveSpeed;
-    public int patrolDestination;
     public enum enemyState
     {
         Patrol,
@@ -16,44 +13,19 @@ public class EnemyAI : MonoBehaviour
         Aggro
     }
     
-    public Animator animator;
 
     public enemyState state;
     void Start()
     {
-        state = enemyState.Patrol;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //switch statement which controls the state of the enemy
+
         switch (state)
         {
             case enemyState.Patrol:
-
-                if (patrolDestination == 0)
-                {
-                    transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed *Time.deltaTime);
-                    if (Vector2.Distance(transform.position, patrolPoints[0].position) <.2f)
-                    {
-                        transform.localScale = new Vector3(-1, 1, 1);
-
-                        patrolDestination = 1;
-
-                    }
-                }
-
-                if (patrolDestination == 1)
-                {
-                    transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
-                    if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
-                    {
-                        transform.localScale = new Vector3(1, 1, 1);
-                        patrolDestination = 0;
-
-                    }
-                }
 
                 break;
             case enemyState.Suspicious:
@@ -68,12 +40,6 @@ public class EnemyAI : MonoBehaviour
      
     }
 
-    public void NoticeSomething(Transform suspiciousObject)
-    {
-        state = enemyState.Suspicious;
-        transform.position = Vector2.MoveTowards(transform.position, suspiciousObject.position, moveSpeed * Time.deltaTime);
-
-    }
     private void SpotPlayer()
     {
 
